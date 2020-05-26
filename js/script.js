8bfce04a89f841b309188a9e0337ac0133ffe12d
Segura <segura-deluna@mail.ru>
2020-05-26 15:09:39 +0300

    добавлено переключение изображений по наведению

--- js/script.js
+++ js/script.js
@@ -9,33 +9,37 @@ window.addEventListener('DOMContentLoaded', (event) => {
 		hamburger.classList.toggle('open');
 	});
 
-	document.addEventListener('click', event => {
 
+	document.addEventListener('click', event => {
 		if (!event.target.closest('.left-menu')) {
 			leftMenu.classList.remove('openMenu');
 			hamburger.classList.remove('open');
 		}
-
 	});
 
 
 	leftMenu.addEventListener('click', event => {
-
 		const target = event.target;
 		const dropdown = target.closest('.dropdown');
-
 		if (dropdown) {
 			dropdown.classList.toggle('active');
 			leftMenu.classList.add('openMenu');
 			hamburger.classList.add('open');
 		}
-
 	});
 
 
-
-
-
+	const changeImage = document.querySelectorAll('.tv-card__img');
+	changeImage.forEach((img) => {
+		const image = img.src;
+		const imageDuble = img.getAttribute('data-backdrop');
+		img.addEventListener('mouseover', () => {
+			img.src = imageDuble;
+		})
+		img.addEventListener('mouseout', () => {
+			img.src = image;
+		})
+	});
 
 });
 
